4c816f1cfc4b90e92caea1f530f34e0831c79b45
F3rchu987 <165219751+F3rchu987@users.noreply.github.com>
2026-07-23 21:06:33 -0300

    Delete ARCHIVOS ALUMNOS directory

--- ARCHIVOS ALUMNOS/Pasaje Basico- Gabriel Cabrera.py
+++
@@ -1,8 +0,0 @@
-precio=int(input("Ingrese el precio del combustible por litro:"))
-pasajero=int(input("Ingrese la cantidad de pasajeros:"))
-consumo=int(input("Ingrese el consumo del vehículo (lts/km):"))
-distancia=int(input("Ingrese la distancia del viaje (km):"))
-costototal=consumo*distancia*precio
-costoxpersona=costototal/pasajero
-print("El costo total del combustible es: ", costototal)
-print("Cada pasajero debe pagar: ", costoxpersona)
